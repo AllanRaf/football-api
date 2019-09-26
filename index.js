49466e61571a744894138a2ce1b61b268c67d7f2
AllanRaf <rafale.uk@googlemail.com>
2019-09-26 16:23:47 +0200

    Define team model

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const db = require('./db')
+const team = require('./team/model')
 const app = express()
 const port = process.env.PORT || 4000
+
+//app.use(team)
 
 app.listen(port, console.log(`listening on port: ${port}`))
