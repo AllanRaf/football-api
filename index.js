88ae16f11d9cd961b1d6fe5b5825cc29e651d40a
AllanRaf <rafale.uk@googlemail.com>
2019-09-26 16:06:25 +0200

    Add database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
+const db = require('./db')
 const app = express()
 const port = process.env.PORT || 4000
 
-app.listen(port, console.log(`listening to port: ${port}`))
+app.listen(port, console.log(`listening on port: ${port}`))
