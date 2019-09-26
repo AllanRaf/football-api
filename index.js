7977e6eb7929c51f721409efccb15a8bcd8021fd
AllanRaf <rafale.uk@googlemail.com>
2019-09-26 17:28:05 +0200

    Add POST method

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const teamRouter = require('./team/router')
+const bodyParser = require('body-parser')
+
+const jsonParser = bodyParser.json()
+
 const app = express()
+app.use(jsonParser)
 const port = process.env.PORT || 4000
 
 app.use(teamRouter)
--- team/router.js
+++ team/router.js
@@ -1,12 +1,20 @@
 const { Router } = require('express')
 const Team = require('./model')
 
+
 const router = new Router()
 
 router.get('/team', (request, response, next) => {
             Team.findAll()
             .then(team => response.send(team))
             .catch(err => next(err))
+    })
+
+router.post('/team', (request, response, error) =>{
+        Team.create(
+            request.body
+        )
+        .then(team => response.status(201).send(team))
     })
 
 module.exports = router
