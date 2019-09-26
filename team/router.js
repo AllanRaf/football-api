e3521c0ebd3248067a2151943b606cfc3fdd8232
AllanRaf <rafale.uk@googlemail.com>
2019-09-26 17:50:50 +0200

    Add another route for GET method

--- team/router.js
+++ team/router.js
@@ -17,4 +17,14 @@ router.post('/team', (request, response, error) =>{
         .then(team => response.status(201).send(team))
     })
 
+router.get('/team/:id', (request, response, next)=>{
+    Team.findOne({
+        where:{
+            id: request.params.id
+        }
+    })
+    .then(team => response.status(201).send(team))
+    .catch(err => next(err))
+})
+
 module.exports = router
